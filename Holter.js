5a427b3ed3761ef34a2bae05acff4fdde11b913f
S9576903W <s9576903w@localhost.localdomain>
2025-11-06 11:47:28 +0100

    Creazione primi Route di Prova

--- Holter.js
+++ Holter.js
@@ -4,9 +4,17 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content type: ': 'text/plain'});
+if (req.url === '/' && req.method === 'GET'){
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
 
     res.end('\nHello World');
+}
+
+if (req.url === '/about' && req.method === 'GET'){
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+
+    res.end('\nCiao siamo Ibba e Zappia questa è la nostra pagina relativa al tuo Holter!! \n Il tuo Holter funziona grazie a noi.');
+}
 
 });
 
