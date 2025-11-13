688b793539e897f4128100da76ee0b6e5e2d9adc
S9576903W <s9576903w@localhost.localdomain>
2025-11-13 11:51:08 +0100

    Lettura json da finire

--- Holter.js
+++ Holter.js
@@ -1,20 +1,34 @@
 const http = require('http');
-
+const fs = require('fs').promises;
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const server =  http.createServer(async (req, res) => {
 
 if (req.url === '/' && req.method === 'GET'){
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-
-    res.end('\nHello World');
-}
-
-if (req.url === '/about' && req.method === 'GET'){
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-
+    res.statusCode = 200;
+    res.end('\nHello World')
+} else if (req.url === '/about' && req.method === 'GET'){
+    res.statusCode = 200;
     res.end('\nCiao siamo Ibba e Zappia questa è la nostra pagina relativa al tuo Holter!! \n Il tuo Holter funziona grazie a noi!');
-}
+} else if (req.url === '/dashboard' && req.method === 'GET'){
+    res.statusCode = 200;
+    res.end('\nciao');
+} else if (req.url === '/paziente/elenco' && req.method === 'GET'){
+    let data = await fs.readFile('Pazienti.json','utf8');
+    let pazienti = JSON.parse(data);
+    res.statusCode = 200;
+    res.end(pazienti);
+}else {
+    res.statusCode = 404;
+    res.end('404 Not Found');
+  }
+
+/*if (req.url === '/paziente/***' && req.method === 'GET'){
+    res.statusCode = 200;
+    res.end();
+}*/
+
+
 
 });
 
